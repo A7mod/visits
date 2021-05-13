c513f1f142e01f78d4272555885fbe85521ee9f5
A7mod <aamodkrishna@gmail.com>
2021-05-13 19:32:48 +0530

    explaining what is exit status

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ client.set('visits', 0);
 
 app.get('/', (req, res) => {
     process.exit(0);           // wait will explain 
+    // ok, what this does is, this is a status code for 'exiting and everything is ok'
+    // we're just giving it a false status to exit, which is nicen't
     client.get('visits', (err, visits) => {
         res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1);
